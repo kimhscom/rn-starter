ebded0f2e7df9ce647817138b5f5157111a8dd19
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-09-06 20:05:31 +0900

    #32 Parent-Child Relationships

---
+++ src/components/ImageDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ImageDetail = () => {
+  return <Text>Show image of forest</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default ImageDetail;
