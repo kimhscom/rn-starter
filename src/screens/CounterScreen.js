865984dec082a5516899dc27f0001eee81d24420
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-09-07 13:16:43 +0900

    #40 Screen Boilerplate

---
+++ src/screens/CounterScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const CounterScreen = () => {
+  return (
+    <View>
+      <Text>CounterScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default CounterScreen;
