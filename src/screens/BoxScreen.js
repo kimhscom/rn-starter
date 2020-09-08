06e8aa6c840d768458ce60831591633c62de5b7c
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-09-08 14:36:42 +0900

    #68 Layout with React Native

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const BoxScreen = () => {
+  return (
+    <View>
+      <Text>Box Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default BoxScreen;
